e3e289b66a140f790a408a7c2de408ee37d0d003
Xaver Hugl <xaver.hugl@gmail.com>
2022-09-20 14:18:56 +0200

    wayland/datadevice: support fractional scaling

--- src/wayland/datadevice_interface.cpp
+++ src/wayland/datadevice_interface.cpp
@@ -313,7 +313,7 @@ void DataDeviceInterface::updateDragTarget(SurfaceInterface *surface, quint32 se
     } else if (d->seat->isDragTouch()) {
         pos = d->seat->dragSurfaceTransformation().map(d->seat->firstTouchPointPosition());
     }
-    d->send_enter(serial, surface->resource(), wl_fixed_from_double(pos.x()), wl_fixed_from_double(pos.y()), offer ? offer->resource() : nullptr);
+    d->send_enter(serial, surface->resource(), wl_fixed_from_double(pos.x() * surface->compositorToClientScale()), wl_fixed_from_double(pos.y() * surface->compositorToClientScale()), offer ? offer->resource() : nullptr);
     if (offer) {
         auto matchOffers = [dragSource, offer] {
             const DataDeviceManagerInterface::DnDAction action = chooseDndAction(dragSource, offer);
