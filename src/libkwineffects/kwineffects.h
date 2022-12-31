3dfa5f94acde2169cfdaedbf382abb2fc0223103
Luis Bocanegra <luisbocanegra17b@gmail.com>
2022-12-31 05:09:40 -0600

    Revert "effects: Drop WindowPaintData.shader property"
    This reverts commit 3aa635430bc67abd4d1a721153b9969b929ec57d.

--- src/libkwineffects/kwineffects.h
+++ src/libkwineffects/kwineffects.h
@@ -80,6 +80,7 @@ class OffscreenQuickView;
 class EffectScreen;
 class Effect;
 class WindowQuad;
+class GLShader;
 class WindowQuadList;
 class WindowPrePaintData;
 class WindowPaintData;
@@ -3431,6 +3432,11 @@ public:
      */
     std::optional<qreal> renderTargetScale() const;
     void setRenderTargetScale(qreal scale);
+
+    /**
+     * Shader to be used for rendering, if any.
+     */
+    GLShader *shader;
 
 private:
     const std::unique_ptr<WindowPaintDataPrivate> d;
